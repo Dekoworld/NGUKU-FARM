2d610894b0418268aae816287d6df601fc55d453
Dekoworld <karituderrick@gmail.com>
2019-10-09 15:35:40 +0300

    add rows and style them appropriatly

--- js/scripts.js
+++ js/scripts.js
@@ -1,10 +1,26 @@
+// $(document).ready(function(){
+//     $("#btn-login").click(function(){
+//         $("#form-popup").show();
+// });
+// });
+// $(document).ready(function(){
+//     $("#btn-signin").click(function(){
+//         $("#form-popup2").show();
+// });
+
+
 $(document).ready(function(){
-    $("#btn-login").click(function(){
-        $("#form-popup").show();
-});
-});
-$(document).ready(function(){
-    $("#btn-signin").click(function(){
-        $("#form-popup2").show();
-});
-});
+    $(".design-c").click(function(){
+      $("#design-image").toggle()
+      $("#design-para").toggle()
+    });
+    $(".devpt-c").click(function(){
+      $("#devpt-image").toggle();
+      $("#devpt-para").toggle();
+    });
+  
+    $(".prdt-c").click(function(){
+      $("#prdt-image").toggle();
+      $("#prdt-para").toggle();
+    });
+  
