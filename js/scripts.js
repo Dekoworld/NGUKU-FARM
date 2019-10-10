2bf8bafcc1e40e4a7af6cfae995c1fe4c02d83d9
oodennis20 <oodennis20@gmail.com>
2019-10-10 21:33:58 +0300

    add a mailchimp

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,33 @@
-<<<<<<< HEAD
+$('#contactus-form').bind('submit', function(event) {
+    event.preventDefault(); //prevent page refresh
+  
+    var name  = $('[name="fist-name"]').val();//get name field value
+    var email = $('[name="email"]').val();//get email field value
+    var message = $('[name="message"]').val();//get email field value
+    var url =   "https://us20.api.mailchimp.com/2.0/lists/subscribe.json?"
+              +  "apikey=509f1576565b913cce3554b7c756d303-us20&id=3563379fa3"//API KEY  
+              +  "&email[email]=" + email //email address registering
+              +  "&merge_vars[NAME]=" + name //name we're registering
+              +  "&merge_vars[MESSAGE]=" + message//message
+              +  "&double_optin=true" //add to list even if email is invalid
+              +  "&send_welcome=true"; //send an email notification to new subscriber
+    $.ajax({
+        type: "POST",
+        url: url,
+        contentType: 'application/json; charset=utf-8',
+        dataType: 'json',
+  
+        success: function(data) {
+          $('#contactus-form').html(name + " we have received your message. Thank you fro reaching out to us.");
+          alert("success");
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+          console.log(errorThrown);
+        }
+    });  //End of ajax
+    alert(name + " we have received your message. Thank you fro reaching out to us.")
+  });  //End of click bind
+
 var slideIndex = 0;
 showSlides();
 
@@ -41,65 +70,3 @@ $(document).ready(function(){
 //     $("#btn-signin").click(function(){
 //         $("#form-popup2").show();
 // });
-$('#contactus-form').bind('submit', function(event) {
-
-  event.preventDefault(); //prevent page refresh
-
-  var name  = $('[name="fist-name"]').val();//get name field value
-  var email = $('[name="email"]').val();//get email field value
-  var message = $('[name="message"]').val();//get email field value
-  var url =   "https://us20.api.mailchimp.com/2.0/lists/subscribe.json?"
-            +  "apikey=509f1576565b913cce3554b7c756d303-us20&id=3563379fa3"//API KEY  
-            +  "&email[email]=" + email //email address registering
-            +  "&merge_vars[NAME]=" + name //name we're registering
-            +  "&merge_vars[MESSAGE]=" + message//message
-            +  "&double_optin=true" //add to list even if email is invalid
-            +  "&send_welcome=true"; //send an email notification to new subscriber
-  $.ajax({
-      type: "POST",
-      url: url,
-      contentType: 'application/json; charset=utf-8',
-      dataType: 'json',
-
-      success: function(data) {
-        $('#contactus-form').html(name + " we have received your message. Thank you fro reaching out to us.");
-        alert("success");
-      },
-      error: function(jqXHR, textStatus, errorThrown) {
-        console.log(errorThrown);
-      }
-  });  //End of ajax
-  alert(name + " we have received your message. Thank you fro reaching out to us.")
-});  //End of click bind
-  
-=======
-$('#contactus-form').bind('submit', function(event) {
-    event.preventDefault(); //prevent page refresh
-  
-    var name  = $('[name="fist-name"]').val();//get name field value
-    var email = $('[name="email"]').val();//get email field value
-    var message = $('[name="message"]').val();//get email field value
-    var url =   "https://us20.api.mailchimp.com/2.0/lists/subscribe.json?"
-              +  "apikey=509f1576565b913cce3554b7c756d303-us20&id=3563379fa3"//API KEY  
-              +  "&email[email]=" + email //email address registering
-              +  "&merge_vars[NAME]=" + name //name we're registering
-              +  "&merge_vars[MESSAGE]=" + message//message
-              +  "&double_optin=true" //add to list even if email is invalid
-              +  "&send_welcome=true"; //send an email notification to new subscriber
-    $.ajax({
-        type: "POST",
-        url: url,
-        contentType: 'application/json; charset=utf-8',
-        dataType: 'json',
-  
-        success: function(data) {
-          $('#contactus-form').html(name + " we have received your message. Thank you fro reaching out to us.");
-          alert("success");
-        },
-        error: function(jqXHR, textStatus, errorThrown) {
-          console.log(errorThrown);
-        }
-    });  //End of ajax
-    alert(name + " we have received your message. Thank you fro reaching out to us.")
-  });  //End of click bind
->>>>>>> add mailchimp
