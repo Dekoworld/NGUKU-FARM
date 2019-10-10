0bc1188c7dc399af29872b50f4cec3f81e09146b
Dekoworld <karituderrick@gmail.com>
2019-10-10 04:58:41 +0300

    style page footer

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,22 @@
-// $(document).ready(function(){
-//     $("#btn-login").click(function(){
-//         $("#form-popup").show();
-// });
-// });
-// $(document).ready(function(){
-//     $("#btn-signin").click(function(){
-//         $("#form-popup2").show();
-// });
+var slideIndex = 0;
+showSlides();
 
-
+function showSlides() {
+  var i;
+  var slides = document.getElementsByClassName("mySlides");
+  var dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+}
 $(document).ready(function(){
     $(".design-c").click(function(){
       $("#design-image").toggle()
@@ -23,4 +31,13 @@ $(document).ready(function(){
       $("#prdt-image").toggle();
       $("#prdt-para").toggle();
     });
+    // $(document).ready(function(){
+//     $("#btn-login").click(function(){
+//         $("#form-popup").show();
+// });
+// });
+// $(document).ready(function(){
+//     $("#btn-signin").click(function(){
+//         $("#form-popup2").show();
+// });
   
